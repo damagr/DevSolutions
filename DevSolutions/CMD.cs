ccdab1ee880bbe7305b4ad1cdb02192b49523a9e
damagr <damiamartinez94@gmail.com>
2020-12-14 20:15:23 +0100

    Solucionado un bug que provocaba que no se mostrase correctamente la ejecución de los compandos en tiempo real.

--- DevSolutions/CMD.cs
+++ DevSolutions/CMD.cs
@@ -1,83 +1,76 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Text;
 
 namespace DevSolutions
 {
     public static class CMD
     {
-        private static StringBuilder cmdOutput = null;
-        private static int numOutputLines = 0;
-
         /* FUNCIÓN QUE EJECUTA EL COMANDO POR EL CMD */
         public static void ExecuteCommand(string command)
         {
-            //Inicializamos el proceso
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-
-            // Asignamos la propiedad a false para evitar la redirección
-            cmd.StartInfo.UseShellExecute = false;
-
-            // Redireccionamos la salida por pantalla original por una lectura asincrona utilizando un event handler
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmdOutput = new StringBuilder();
-
-            // Asignamos nuestro event handler a una lectura asincrona para sacar la ejecución del programa por pantalla
-            cmd.OutputDataReceived += CmdOutputHandler;
-
-            // Recireccionamos la entrada de datos de forma sincrona.
-            cmd.StartInfo.RedirectStandardInput = true;
-
-            // Inicializamos el proceso
-            cmd.Start();
-
-            // Inicializamos la lectura asincrona de las lecturas por pantalla del programa
-            cmd.BeginOutputReadLine();
-
-            // Pasamos el comando por la linea de comandos
-            cmd.StandardInput.WriteLine(command);
-            cmd.StandardInput.Flush();
-
-            // A medida que van saliendo nuevas lineas de ejecución las mostramos
-            if(numOutputLines > 0)
+            try
             {
-                Console.WriteLine(cmdOutput);
+                Process cmd = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/K " + command,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                updateStatusExecution("***COMMAND RECEIVED: " + command);
+                updateStatusExecution("***ANSWER:");
+                cmd.Start();
+                while (!cmd.StandardOutput.EndOfStream)
+                {
+                    string line = cmd.StandardOutput.ReadLine();
+                    updateStatusExecution(line);
+                }
             }
-
-            // Finalizamos la entrada de datos y cerramos la ejecución
-            cmd.StandardInput.Close();
-            cmd.WaitForExit();
+            catch (Exception e)
+            {
+                updateStatusExecution("***Error while executing '" + command + "'");
+                updateStatusExecution("***Exception: '" + e.ToString());
+                updateStatusExecution("***Stack Trace: '" + e.StackTrace.ToString());
+            }
         }
-
-        // Event handler encargado de ir cargandose a medida que entran nuevas lineas de ejecución
-        private static void CmdOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        private static void updateStatusExecution(string textR)
         {
-            // Recogemos la información que sale por pantalla
-            if (!String.IsNullOrEmpty(outLine.Data))
-            {
-                numOutputLines++;
-
-                // Añadimos el texto al colector para la salida por pantalla
-                cmdOutput.Append(Environment.NewLine + $"[{numOutputLines}] - {outLine.Data}");
-            }
+            Console.WriteLine(textR);
         }
-
         public static void ExecuteDir(string command)
         {
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.RedirectStandardInput = true;
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmd.StartInfo.CreateNoWindow = false;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
-
-            cmd.StandardInput.WriteLine(command);
-            cmd.StandardInput.Flush();
-            cmd.StandardInput.Close();
-            cmd.WaitForExit();
-            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            try
+            {
+                Process cmd = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/C " + command,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                updateStatusExecution("***COMMAND RECEIVED: " + command);
+                updateStatusExecution("***ANSWER:");
+                cmd.Start();
+                while (!cmd.StandardOutput.EndOfStream)
+                {
+                    string line = cmd.StandardOutput.ReadLine();
+                    updateStatusExecution(line);
+                }
+            }
+            catch (Exception e)
+            {
+                updateStatusExecution("***Error while executing '" + command + "'");
+                updateStatusExecution("***Exception: '" + e.ToString());
+                updateStatusExecution("***Stack Trace: '" + e.StackTrace.ToString());
+            }
         }
     }
 }
